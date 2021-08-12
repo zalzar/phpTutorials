96aef6fc70ed1658b0200cbf90eb1d385ce714c2
Tutorial <ali.zalzar@gmail.com>
2021-08-12 08:44:44 +0300

    delivering from sql

--- index.php
+++ index.php
@@ -27,7 +27,25 @@
 <html lang="en">
     <?php include('templates/header.php') ?>
 
+    <h4 class="center">Pizzas</h4>
 
+    <div class="container">
+        <div class="row">
+            <?php  foreach ($pizzas as $pizza) { ?>
+                <div class="col s6 md3">
+                    <div class="card z-depth-0">
+                        <div class="card-content center">
+                            <h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
+                            <div><?php echo htmlspecialchars($pizza['ingredients']); ?></div>
+                        </div>
+                        <div class="card-action right-align">
+                            <a href="#" class="brand-text">more info</a>
+                        </div>
+                    </div>
+                </div>
+            <?php }?>
+        </div>
+    </div>
 
     <?php include('templates/footer.php') ?>
 </html>
